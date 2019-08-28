7a33e2c4a5b478530732d0356da7503f5ffd5b93
He Zhang <rbczhang90@gmail.com>
2019-08-28 10:47:37 -0700

    Past projects square style

--- src/containers/Experience/Experience.js
+++ src/containers/Experience/Experience.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import classes from "./Experience.module.css";
+import placeHolder from '../../assets/images/crane.svg';
 
 export class Experience extends Component {
     
     render() {
         return (
-            <div>
-                <h1>My experience</h1>
+            <div className={classes.Experience}>
+                <div className={classes.AppsContainer}>
+                    <div className={classes.App}>
+                        <h2 className={classes.appTitle}>Burger Builder</h2>
+                        <img src={placeHolder} alt="placeHolder" className={classes.AppImage}></img>
+                        <div className={classes.listContainer}>
+                        <ul className={classes.list}>
+                            <li>FeatureFeatureFeatureFeatureFeatureFeatureFeature</li>
+                            <li>FeatureFeatureFeatureFeatureFeature</li>
+                            <li>FeatureFeatureFeatureFeatureFeatureFeatureFeatureFeatureFeatureFeature</li>
+                        </ul>     
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
