1f9beba8449646955a2710c87b9e129b62aab009
He Zhang <rbczhang90@gmail.com>
2019-08-29 18:12:00 -0700

    Large screen style for app showcase

--- src/containers/Experience/Experience.js
+++ src/containers/Experience/Experience.js
@@ -7,17 +7,15 @@ export class Experience extends Component {
     render() {
         return (
             <div className={classes.Experience}>
-                <div className={classes.AppsContainer}>
-                    <div className={classes.App}>
-                        <h2 className={classes.appTitle}>Burger Builder</h2>
-                        <img src={placeHolder} alt="placeHolder" className={classes.AppImage}></img>
-                        <div className={classes.listContainer}>
+                <div className={classes.AppContainer}>
+                    <header className={classes.appTitle}>Burger Builder</header>
+                    <img src={placeHolder} alt="placeHolder" className={classes.AppImage}></img>
+                    <div className={classes.listContainer}>
                         <ul className={classes.list}>
-                            <li>FeatureFeatureFeatureFeatureFeatureFeatureFeature</li>
-                            <li>FeatureFeatureFeatureFeatureFeature</li>
-                            <li>FeatureFeatureFeatureFeatureFeatureFeatureFeatureFeatureFeatureFeature</li>
+                            <li className={classes.listItem}>FeatureFeatureFeatureFeatureFeatureFeatureFeature</li>
+                            <li className={classes.listItem}>FeatureFeatureFeatureFeatureFeature</li>
+                            <li className={classes.listItem}>FeatureFeatureFeatureFeatureFeatureFeatureFeatureFeatureFeatureFeature</li>
                         </ul>     
-                        </div>
                     </div>
                 </div>
             </div>
