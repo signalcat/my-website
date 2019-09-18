a582d290daff8e086daea69164280baaac223030
He Zhang <rbczhang90@gmail.com>
2019-09-18 16:10:34 -0700

    Add photos

--- src/containers/Photography/Gallery.js
+++ src/containers/Photography/Gallery.js
@@ -8,15 +8,39 @@ export class Gallery extends Component {
         function importAll(r) {
             return r.keys().map(r);
         }
-        const images = importAll(require.context('../../assets/gallery', false, /\.(png|jpe?g|svg|heic)$/));
+        
+        const imgFood = importAll(require.context('../../assets/gallery/food', false, /\.(png|jpe?g|svg|heic|JPG)$/));
+        const imgNature = importAll(require.context('../../assets/gallery/nature', false, /\.(png|jpe?g|svg|heic|JPG)$/));
+        const imgDogs = importAll(require.context('../../assets/gallery/dogs', false, /\.(png|jpe?g|svg|heic|JPG)$/));
+
         return (
             <div className={classes.Gallery}>
-            <Toolbar></Toolbar>
+                <Toolbar></Toolbar>
+                <h1 className={classes.albumnTitle}>Food</h1>
+                <div className={classes.albumn}> 
                 {
-                    images.map((path, index) => {
+                    imgFood.map((path, index) => {
                         return <img key={index} src={path} alt="pic" className={classes.GalleryImage}/>;
-                    })
-                }
+                    })}
+                </div>
+
+                <h1 className={classes.albumnTitle}>Nature</h1> 
+                <div className={classes.albumn}>
+                {
+                    imgNature.map((path, index) => {
+                        return <img key={index} src={path} alt="pic" className={classes.GalleryImage}/>;
+                    })}
+                </div>
+
+                <h1 className={classes.albumnTitle}>Dogs</h1> 
+                <div className={classes.albumn}>
+                {
+                    imgDogs.map((path, index) => {
+                        return <img key={index} src={path} alt="pic" className={classes.GalleryImage}/>;
+                    })}
+                </div>
+
+
             </div>
         );
     }
